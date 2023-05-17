7836e3f8e555907cdd48f74ca28f8aec0b42e88a
Melvyn Marie <mmarie@c2r7p4.42perpignan.fr>
2023-05-17 13:27:35 +0200

    end

--- Srcs/client.c
+++ Srcs/client.c
@@ -6,7 +6,7 @@
 /*   By: mmarie <mmarie@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 13:29:42 by mmarie            #+#    #+#             */
-/*   Updated: 2023/05/17 10:32:34 by mmarie           ###   ########.fr       */
+/*   Updated: 2023/05/17 13:19:08 by mmarie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,41 @@ void	handler(int	signal)
 	(void)signal;
 }
 
+int check_error(int argc, char **argv)
+{
+		if (argc > 1 && (argv[1][0] < '0' || argv[1][0] > '9'))
+		{
+			ft_printf("Erreur : Le PID doit être un nombre entre 0 et 9.\n");
+			return(0);
+		}
+		if (argc < 2)
+		{
+			ft_printf("Erreur : Le PID n'est pas indique.\n");
+			return(0);
+		}
+
+		if (argc < 3)
+		{
+			ft_printf("Erreur : Le message est manquant.\n");
+			return(0);
+		}
+
+		if (argc > 3)
+		{
+			ft_printf("Erreur : N'oublie les guillemets.\n");
+			return(0);
+		}
+		return(1);
+}
+
 int main(int argc, char **argv)
 {
 	int	pid;
 	int	i;
 	char *mess;
 
+	if (!check_error(argc, argv))
+		return (1);
 	signal(SIGUSR1, handler);
 	signal(SIGUSR2, handler);
 	(void) argc;
@@ -55,31 +84,7 @@ int main(int argc, char **argv)
 		count_bit(pid, mess[i]);
 		i++;
 	}
-	ft_printf("Message received!");
+	ft_printf("Message received!\n");
 	count_bit(pid, 0);
 	return(0);
 }
-
-int check_error(int argc, char **argv)
-	{
-		int	i;
-
-		(void) argc;
-		if (argv[1][0] < '1' || argv[1][0] > '9')
-		{
-			ft_printf("Erreur : Le PID doit être un nombre entre 1 et 9.\n");
-			return(0);
-		}
-
-		if (argv[2] = 0)
-		{
-			printf("rentre un message ");
-			return(0);
-		}
-
-		if (argv[3])
-		{
-			printf("Erreur : N'oublie les guillemets.\n"); // ex : ./test 1823 emzqm eqmzm
-			return(0);
-		}
-	}
